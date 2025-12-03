8a8907828759c90deef772ed94fba36f79a81551
rajat-0045 <150031363+rajat-0045@users.noreply.github.com>
2025-12-03 14:54:38 +0530

    train.py

--- src/train.py
+++ src/train.py
@@ -11,7 +11,7 @@ def main():
     df = pd.read_csv("data/iris.csv")
     X = df.drop("Name", axis=1)
     y = df["Name"]
-
+ 
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42, stratify=y
     )
